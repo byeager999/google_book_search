c1dbffff6a201f99cb8031dd1e81d81004fa0e44
Brian Yeager <rbrianyeager@gmail.com>
2019-12-15 12:42:26 -0500

    basic file structure and book search logic

---
+++ client/src/utils/API.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+export default {
+    search: function(query) {
+        return axios.get(BASEURL + query);
+    },
+
+    save: function(bookData) {
+        console.log("bookData below")
+        console.log(bookData.title)
+        return axios.post("/api/books", bookData);
+
+    },
+
+    getBooks: function() {
+        console.log("getBooks from API.js");
+        return axios.get("/api/books")
+    }, 
+
+    deleteBook: function(id) {
+        console.log(id.id)
+        // Not sure why I had to do id.id here. It was coming through as an object instead of just the id. But now it works.
+        // This way it fills out the route correctly.
+        return axios.delete("/api/books/" + id.id);
+    } 
+
+}
+
